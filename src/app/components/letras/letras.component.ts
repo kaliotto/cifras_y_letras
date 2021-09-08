bf98c1f888be79c7a6641e481a35eb7bc14c52c9
msalazar <msalazar@oesia.com>
2021-09-08 16:27:57 +0200

    Letras game implemented

--- src/app/components/letras/letras.component.ts
+++ src/app/components/letras/letras.component.ts
@@ -8,9 +8,21 @@ import { JuegoService } from 'src/app/services/juego.service';
 })
 export class LetrasComponent implements OnInit {
 
-  constructor(private juego:JuegoService) { }
+  consonantes: string[] = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "ñ", "p", "q", "r", "s", "t", "v", "w", "x", "y", "z"];
+  vocales: string[] = ["a", "e", "i", "o", "u"];
+  letras: string[] = [];
+
+  constructor(private juego: JuegoService) { }
 
   ngOnInit(): void {
+  }
+
+  consonante(){
+    this.letras.push(this.consonantes[Math.floor(Math.random()*this.consonantes.length)].toUpperCase());
+  }
+
+  vocal(){
+    this.letras.push(this.vocales[Math.floor(Math.random()*this.vocales.length)].toUpperCase());
   }
 
   jugar() {
