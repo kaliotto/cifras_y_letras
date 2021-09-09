c2b1985700640d5dbca27bff493d1d1b30e9e6ad
msalazar <msalazar@oesia.com>
2021-09-09 09:40:44 +0200

    QuienGana refined

--- src/app/components/shared/quien-gana/quien-gana.component.ts
+++ src/app/components/shared/quien-gana/quien-gana.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { Fase } from 'src/app/enums/fase';
 import { Jugador } from 'src/app/interfaces/jugador';
 import { JuegoService } from 'src/app/services/juego.service';
 
@@ -11,15 +12,19 @@ import { JuegoService } from 'src/app/services/juego.service';
 export class QuienGanaComponent implements OnInit {
   jugadores: Jugador[]=[];
   ganador = new FormControl('');
+  puntuacion = new FormControl('');
+  fases = Fase;
+  fase:Fase | undefined;
 
   constructor(private juego: JuegoService) { }
 
   ngOnInit(): void {
     this.jugadores = this.juego.getJugadores();
+    this.juego.getFase().subscribe(fase => { this.fase = fase });
   }
 
   alerta(){
-    console.log(this.ganador);
-    
+    console.log(this.ganador.value);
+    console.log(this.puntuacion.value);
   }
 }
