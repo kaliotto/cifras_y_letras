71f05d2afac694514cf33b452a5b06788016ced2
kaliotto <kaliotto@gmail.com>
2021-09-19 00:24:08 +0200

    Test for firebase deployment

--- src/app/services/juego.service.ts
+++ src/app/services/juego.service.ts
@@ -19,19 +19,19 @@ export class JuegoService {
 
   jugar() {
 
-    if (this.jugadores.length == 0) {
-      this.jugadores = [
-        { nombre: "mikel", posicion: 1, puntuacion: 0 },
-        { nombre: "vero", posicion: 2, puntuacion: 0 },
-        { nombre: "carol", posicion: 3, puntuacion: 0 },
-        { nombre: "carlos", posicion: 4, puntuacion: 0 },
-        { nombre: "txemi", posicion: 5, puntuacion: 0 },
-        { nombre: "vir", posicion: 6, puntuacion: 0 },
-        { nombre: "jesus", posicion: 7, puntuacion: 0 },
-        { nombre: "nerea", posicion: 8, puntuacion: 0 },
-        { nombre: "adrian", posicion: 9, puntuacion: 0 }
-      ];
-    }
+    // if (this.jugadores.length == 0) {
+    //   this.jugadores = [
+    //     { nombre: "mikel", posicion: 1, puntuacion: 0 },
+    //     { nombre: "vero", posicion: 2, puntuacion: 0 },
+    //     { nombre: "carol", posicion: 3, puntuacion: 0 },
+    //     { nombre: "carlos", posicion: 4, puntuacion: 0 },
+    //     { nombre: "txemi", posicion: 5, puntuacion: 0 },
+    //     { nombre: "vir", posicion: 6, puntuacion: 0 },
+    //     { nombre: "jesus", posicion: 7, puntuacion: 0 },
+    //     { nombre: "nerea", posicion: 8, puntuacion: 0 },
+    //     { nombre: "adrian", posicion: 9, puntuacion: 0 }
+    //   ];
+    // }
 
     this.faseActual = this.etapasDelJuego[this.etapaActual];
     this.etapaActual++;
